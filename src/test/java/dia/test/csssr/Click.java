221b742a89a08068e3e5e7a40e79f3f2d60f7582
Diachkova <alphaomega@rambler.ru>
2018-11-26 10:58:22 +0300

    begin

---
+++ src/test/java/dia/test/csssr/Click.java
@@ -0,0 +1,18 @@
+package dia.test.csssr;
+
+public class Click {
+
+  public static void main(String[] args) {
+    output("Oдин клик на состояние блока ");
+    output("Второй клик на состояние блока ");
+  }
+    public static void output (String operation) {
+      String result = "ок";
+      String outp = operation + result;
+      System.out.println(outp);
+    }
+
+    }
+
+
+
